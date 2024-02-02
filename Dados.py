a05e1b940014ad73f22073b4312fa594d20a5cc8
MatheusVDC19 <158532256+MatheusVDC19@users.noreply.github.com>
2024-02-01 22:37:19 -0300

    Add files via upload

--- Dados.py
+++ Dados.py
@@ -1,18 +1,33 @@
 import pandas as pd
 
+
 class DT:
 
-    def obter_dados(caminho):
+    def extracao_bcb(codigo, data_inicio, data_fim):
 
-        df = pd.read_excel(caminho)
+        url = 'https://api.bcb.gov.br/dados/serie/bcdata.sgs.{}/dados?formato=json&dataInicial={}&dataFinal={}'.format(codigo, data_inicio, data_fim)
+        df = pd.read_json(url)
+        df.set_index('data', inplace = True)
+        df.index.name = 'Ano-Mês'
+        df.index = pd.to_datetime(df.index, dayfirst= True)
 
         return df
+    
+    def unir_DFs(df1, df2, df3):
+
+        df12 = pd.merge(df1, df2, how='inner', on='Ano-Mês')
+        df23 = pd.merge(df2, df3, how='inner', on='Ano-Mês')
+        df = pd.merge(df12, df23, how='inner', on='Ano-Mês' )
+
+        return df
+    
 
     def ult_dado(coluna, df):
 
         ult_dado = df[coluna].iloc[-1]
 
         return ult_dado
+    
     
     def var_dado(coluna, df):
 
--- Dash.py
+++ Dash.py
@@ -1,11 +1,37 @@
 import streamlit as st
 import Dados as dados
 
-df = dados.DT.obter_dados("Dados.xlsx")
+data_inicio = "01/01/2004"
+data_fim = "01/01/2024"
+
+
+#Baixando Dados via API
+df_IPCA = dados.DT.extracao_bcb(13522, data_inicio, data_fim)
+df_IPCA.rename(columns={'valor': 'IPCA'}, inplace = True)
+
+df_SELIC = dados.DT.extracao_bcb(432, data_inicio, data_fim)
+df_SELIC.rename(columns={'valor': 'SELIC'}, inplace = True)
+
+df_CAMB = dados.DT.extracao_bcb(3697, data_inicio, data_fim)
+df_CAMB.rename(columns={'valor': 'CAMB'}, inplace = True)
+
+#Unindo os Dataframes
+df = dados.DT.unir_DFs( df_SELIC, df_IPCA, df_CAMB)
+
+
+#Tratando Coluna duplicada
+df.drop(columns=['IPCA_x'], inplace= True)
+df.rename(columns={'IPCA_y' : 'IPCA'}, inplace= True)
+df['Ano-Mês'] = df.index
+df.info()
+
+#Exportando dados em Excel
+df.to_excel('Dados.xlsx')
+
 
 st.header("Panorama Macro")
 
-st.line_chart(df, x="Ano-Mês", y="SELIC")
+st.line_chart(df, x='Ano-Mês', y="SELIC")
 
 col1, col2, col3 = st.columns(3)
 
